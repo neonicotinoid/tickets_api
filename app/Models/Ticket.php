a59015524ce162c9ed6600d107a18616a2939ab5
neonicotinoid <skyyyr@gmail.com>
2022-07-30 14:42:09 +0300

    Enum для статуса заявки

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Enums\TicketStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'status' => TicketStatus::class,
+    ];
 }
