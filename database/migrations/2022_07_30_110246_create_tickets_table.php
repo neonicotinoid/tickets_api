5a88de770dfc8d9c46b985d6e37de1cc931967e5
neonicotinoid <skyyyr@gmail.com>
2022-07-30 14:07:40 +0300

    Модель, контроллер и миграции для модели Тикетов

---
+++ database/migrations/2022_07_30_110246_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email');
+            $table->string('status');
+            $table->text('message');
+            $table->text('comment')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
